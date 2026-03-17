799c1d62b6e02c70f7c8fe9e73313764b1cf7c80
Gustavo-Rodrigue <47822733800@info.local>
2026-03-16 21:39:07 -0300

    Estrutura de coleta de dados

---
+++ Codigos/main.py
@@ -0,0 +1,13 @@
+def Coleta():
+    Notas = []
+    dados = []
+    while True:
+        Nome = str(input("Digite o nome do aluno \n >> "))
+        quantidade = int(input("Quantas notas iram constituir a média \n >> "))
+        for i in range(quantidade):
+            Notas.append(input(f"Digite a {i + 1}° nota do aluno (Utilize . para notas não inteiras) \n >> "))
+        break
+    dados.append[Nome, Notas]
+    print(dados)
+
+Coleta()
