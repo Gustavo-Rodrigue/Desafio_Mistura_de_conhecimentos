6ce0371745c6b84eddbd492c75faab8ea12c7aa4
Gustavo-Rodrigue <47822733800@info.local>
2026-03-17 19:53:57 -0300

    Adição do tratamento de dados + testes

--- Codigos/main.py
+++ Codigos/main.py
@@ -1,14 +1,14 @@
 import processamento as pr
 
-Dados = [("Matheus", [10, 8, 9.5, 8]), 
+Dados = [("Math$eus", [10, 8, "j", 8]), 
          ("Beatriz", [8.4, 9.0, 9.1, 9.6]),
-         ("Lucas", [8.5, 9.9, 8.5, 9.5]),
-         ("Mariana", [8.0, 9.7, 8.5, 9.3])]
+         ("Lu@cas", [8.5, 9.9, "", 9.5]),
+         ("Mariana", [8.0, "t", 8.5, 9.3])]
 
 dados = pr.Organizar(Dados)
 
-Nomes = dados["Nomes"]
-Notas = dados["Notas"]
+Nomes = pr.Validar_Nomes(dados["Nomes"])
+Notas = pr.Validar_Notas(dados["Notas"])
 
 print(Nomes)
 print(Notas)
